c52126894349b63f9f34e93fb2723b73f7baee41
Krecik <dzawisza00@gmail.com>
2024-05-04 16:10:25 +0200

    Add Ilosc_Biletow_page Delete not using files

--- test.py
+++ test.py
@@ -9,9 +9,27 @@ width_image = 30
 height_image = 10
 white = 'white'
 
-def whiteLabel():
-    return tk.Label(frame_center, width=width_of_label, height=height_of_label,
-                      background=white, padx=0, pady=0, highlightthickness="0")
+
+class SingletonMeta(type):
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            instance = super().__call__(*args, **kwargs)
+            cls._instances[cls] = instance
+        return cls._instances[cls]
+
+
+class Bilet(metaclass=SingletonMeta):
+    ilosc = 0
+    is_normalne = None
+
+    def change_count(self, how_many):
+        print("Before ", self.ilosc)
+        if (self.ilosc + how_many >= 0):
+            self.ilosc += how_many
+            print(self.ilosc)
+
 
 class tkinterApp(tk.Tk):
 
@@ -32,11 +50,11 @@ class tkinterApp(tk.Tk):
 
         # iterating through a tuple consisting
         # of the different page layouts
-        for F in (StartPage, Bilet_page):
+        for F in (StartPage, Bilet_page, Ilosc_Biletow_page, Pay_page):
             frame = F(container, self)
 
             # initializing frame of that object from
-            # startpage, page1, page2 respectively with
+            # startpage, bilet_page respectively with
             # for loop
             self.frames[F] = frame
 
@@ -60,6 +78,7 @@ class StartPage(tk.Frame):
         self.controller = controller
         self.white = "white"
         self.image_button = tk.PhotoImage(file=r".\button.png")
+        self.image = tk.PhotoImage(file=r".\image1.png")
 
         self.create()
 
@@ -67,14 +86,13 @@ class StartPage(tk.Frame):
         frame = self.frames[cont]
         frame.tkraise()
 
-    #center panel of center panel
-    def center(self):
-        m0 = tk.Label(frame_center, width=width_of_label*3, height=height_of_label*5 + 1,
-                      background="lightblue",
-                      text="Ekran startowy", padx=0, pady=0)
-        m0.grid(row=0, column=0)
+    # left panel with buttons
+    def create_left(self):
+        frame_left = tk.Frame(self, width=100, padx=0, pady=0)
+        self.buttons(frame_left)
+        frame_left.grid(row=0, column=0, padx=0, pady=0)
 
-    #center panel
+        # center panel
     def create_center(self):
         global frame_center
         frame_center = tk.Frame(self,
@@ -82,21 +100,24 @@ class StartPage(tk.Frame):
         self.center()
         frame_center.grid(row=0, column=1, padx=0, pady=0)
 
-    #left panel with buttons
-    def create_left(self):
-        frame_left = tk.Frame(self, width=100, padx=0, pady=0)
-        self.buttons(frame_left)
-        frame_left.grid(row=0, column=0, padx=0, pady=0)
-
-    #right panel with button
+        # right panel with button
     def create_right(self):
         frame_right = tk.Frame(self, width=100, padx=0, pady=0)
         self.buttons(frame_right)
         frame_right.grid(row=0, column=2, padx=0, pady=0)
 
-    def buttons(self, frame):
+    #center panel of center panel
+    def center(self):
+        m0 = tk.Label(frame_center,padx=0, pady=0, image=self.image)
+        m0.grid(row=0, column=0)
 
-        image = self.image_button.zoom(19, 19).subsample(39)
+
+
+    def buttons(self, frame):
+        width_image = 20
+        height_image = 7
+
+        image = self.image_button.zoom(10, 10).subsample(39)
 
         b0 = tk.Label(frame, width=width_image, height=height_image)
         b1_label = tk.Label(frame, width=width_image, height=height_image)
@@ -133,22 +154,30 @@ class Bilet_page(tk.Frame):
         self.controller = controller
         self.white = "white"
         self.image_button = tk.PhotoImage(file=r".\button.png")
+        self.image = tk.PhotoImage(file=r".\image2.png")
+
         self.create()
 
     def show_frame(self, cont):
         frame = self.frames[cont]
         frame.tkraise()
 
-    def buttons(self, frame):
-        width_image = 30
-        height_image = 10
 
-        image = self.image_button.zoom(19, 19).subsample(39)
+    def change_page(self, choose):
+        bilet = Bilet()
+        bilet.is_normalne = choose
+        self.controller.show_frame(Ilosc_Biletow_page)
+
+    def buttons(self, frame, choose):
+        width_image = 20
+        height_image = 7
+
+        image = self.image_button.zoom(10, 10).subsample(40)
 
         b0 = tk.Label(frame, width=width_image, height=height_image)
         b1_label = tk.Label(frame, width=width_image, height=height_image)
         b1 = tk.Button(b1_label, image=image,
-                       command=lambda: self.controller.show_frame(StartPage))
+                       command=lambda: self.change_page(choose))
         b1.image = image
 
         b1.grid()
@@ -167,70 +196,196 @@ class Bilet_page(tk.Frame):
         b3_label.grid(row=3, column=0)
         b4.grid(row=4, column=0)
 
-    def left(self):
-        l0 = whiteLabel()
-        l1 = tk.Label(frame_center, width=width_of_label, height=height_of_label, background="lightblue",
-                      text="Bilet Ulgowy")
-        l2 = whiteLabel()
-        l3 = tk.Label(frame_center, width=width_of_label, height=height_of_label, background="lightblue")
-        l4 = whiteLabel()
-        l0.grid(row=0, column=0)
-        l1.grid(row=1, column=0)
-        l2.grid(row=2, column=0)
-        l3.grid(row=3, column=0)
-        l4.grid(row=4, column=0)
-
     def center(self):
-        m0 = whiteLabel()
-        m1 = whiteLabel()
-        m2 = whiteLabel()
-        m3 = whiteLabel()
-        m4 = whiteLabel()
-        m0.grid(row=0, column=1)
-        m1.grid(row=1, column=1)
-        m2.grid(row=2, column=1)
-        m3.grid(row=3, column=1)
-        m4.grid(row=4, column=1)
+        m0 = tk.Label(frame_center, padx=0, pady=0, image=self.image)
+        m0.grid(row=0, column=0)
 
-    def right(self):
-        r0 = whiteLabel()
-        r1 = tk.Label(frame_center, width=width_of_label, height=height_of_label, background="lightblue",
-                      text="Bilet Normalny")
-        r2 = whiteLabel()
-        r3 = tk.Label(frame_center, width=width_of_label, height=height_of_label, background="lightblue")
-        r4 = whiteLabel()
-
-        r0.grid(row=0, column=2)
-        r1.grid(row=1, column=2)
-        r2.grid(row=2, column=2)
-        r3.grid(row=3, column=2)
-        r4.grid(row=4, column=2)
 
     def create_center(self):
         global frame_center
         frame_center = tk.Frame(self, highlightbackground="black",
                                 highlightthickness="5", padx=0, pady=0)
-        self.left()
         self.center()
-        self.right()
         frame_center.grid(row=0, column=1)
 
     def create_right(self):
-        frame_right = tk.Frame(self, width=100, padx=0, pady=0)
-        self.buttons(frame_right)
+        frame_right = tk.Frame(self, padx=0, pady=0)
+        self.buttons(frame_right, True)
         frame_right.grid(row=0, column=2)
 
     def create_left(self):
-        frame_left = tk.Frame(self, width=100, padx=0, pady=0)
-        self.buttons(frame_left)
+        frame_left = tk.Frame(self, padx=0, pady=0)
+        self.buttons(frame_left, False)
         frame_left.grid(row=0, column=0)
 
     def create(self):
         self.create_left()
         self.create_center()
         self.create_right()
-        # self.frame.pack()
 
+class Ilosc_Biletow_page(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.frames = None
+        self.controller = controller
+        self.white = "white"
+        self.image_button = tk.PhotoImage(file=r".\button.png")
+        self.image = tk.PhotoImage(file=r".\image3.png")
+
+        self.create()
+
+    def show_frame(self, cont):
+        frame = self.frames[cont]
+        frame.tkraise()
+
+    def change_count(self, how_many):
+        bilet = Bilet()
+        bilet.change_count(how_many)
+
+    def change_page(self, page):
+        if page == -1:
+            self.controller.show_frame(Bilet_page)
+        else:
+            self.controller.show_frame(Pay_page)
+
+    def buttons(self, frame, how_many_to_change):
+        width_image = 20
+        height_image = 7
+
+        image = self.image_button.zoom(10, 10).subsample(39)
+
+        b0 = tk.Label(frame, width=width_image, height=height_image)
+        b1_label = tk.Label(frame, width=width_image, height=height_image)
+        b1 = tk.Button(b1_label, image=image,
+                       command=lambda: self.change_count(how_many_to_change))
+        b1.image = image
+
+        b1.grid()
+
+        b2 = tk.Label(frame, width=width_image, height=height_image)
+        b3_label = tk.Label(frame, width=width_image, height=height_image)
+        b3 = tk.Button(b3_label, image=image,
+                       command=lambda: self.change_page(how_many_to_change))
+
+        b3.grid()
+
+        b4 = tk.Label(frame, width=width_image, height=height_image)
+
+        b0.grid(row=0, column=0)
+        b1_label.grid(row=1, column=0)
+        b2.grid(row=2, column=0)
+        b3_label.grid(row=3, column=0)
+        b4.grid(row=4, column=0)
+
+    def center(self):
+        m0 = tk.Label(frame_center, padx=0, pady=0, image=self.image)
+        m0.grid(row=0, column=0)
+
+
+    def create_center(self):
+        global frame_center
+        frame_center = tk.Frame(self, highlightbackground="black",
+                                highlightthickness="5", padx=0, pady=0)
+        self.center()
+        frame_center.grid(row=0, column=1)
+
+    def create_right(self):
+        frame_right = tk.Frame(self, padx=0, pady=0)
+        self.buttons(frame_right, 1)
+        frame_right.grid(row=0, column=2)
+
+    def create_left(self):
+        frame_left = tk.Frame(self, padx=0, pady=0)
+        self.buttons(frame_left, -1)
+        frame_left.grid(row=0, column=0)
+
+    def create(self):
+        self.create_left()
+        self.create_center()
+        self.create_right()
+
+
+class Pay_page(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.frames = None
+        self.controller = controller
+        self.white = "white"
+        self.image_button = tk.PhotoImage(file=r".\button.png")
+        self.image = tk.PhotoImage(file=r".\image3.png")
+
+        self.create()
+
+    def show_frame(self, cont):
+        frame = self.frames[cont]
+        frame.tkraise()
+
+    def change_count(self, how_many):
+        bilet = Bilet()
+        bilet.change_count(how_many)
+
+    def change_page(self, page):
+        if page == -1:
+            self.controller.show_frame(Bilet_page)
+        else:
+            self.controller.show_frame(Pay_page)
+
+    def buttons(self, frame, how_many_to_change):
+        width_image = 20
+        height_image = 7
+
+        image = self.image_button.zoom(10, 10).subsample(39)
+
+        b0 = tk.Label(frame, width=width_image, height=height_image)
+        b1_label = tk.Label(frame, width=width_image, height=height_image)
+        b1 = tk.Button(b1_label, image=image,
+                       command=lambda: self.change_count(how_many_to_change))
+        b1.image = image
+
+        b1.grid()
+
+        b2 = tk.Label(frame, width=width_image, height=height_image)
+        b3_label = tk.Label(frame, width=width_image, height=height_image)
+        b3 = tk.Button(b3_label, image=image,
+                       command=lambda: self.change_page(how_many_to_change))
+
+        b3.grid()
+
+        b4 = tk.Label(frame, width=width_image, height=height_image)
+
+        b0.grid(row=0, column=0)
+        b1_label.grid(row=1, column=0)
+        b2.grid(row=2, column=0)
+        b3_label.grid(row=3, column=0)
+        b4.grid(row=4, column=0)
+
+    def center(self):
+        m0 = tk.Label(frame_center, padx=0, pady=0, image=self.image)
+        m0.grid(row=0, column=0)
+
+    def create_center(self):
+        global frame_center
+        frame_center = tk.Frame(self, highlightbackground="black",
+                                highlightthickness="5", padx=0, pady=0)
+        self.center()
+        frame_center.grid(row=0, column=1)
+
+    def create_right(self):
+        frame_right = tk.Frame(self, padx=0, pady=0)
+        self.buttons(frame_right, 1)
+        frame_right.grid(row=0, column=2)
+
+    def create_left(self):
+        frame_left = tk.Frame(self, padx=0, pady=0)
+        self.buttons(frame_left, -1)
+        frame_left.grid(row=0, column=0)
+
+    def create(self):
+        self.create_left()
+        self.create_center()
+        self.create_right()
 
 # Driver Code
 app = tkinterApp()
